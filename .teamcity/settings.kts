7508e996fc19c7fb232fac84843c3adbe8e8392b
check <check@example.com>
2023-10-08 00:30:35 +0300

    for check

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,8 +1,10 @@
 import jetbrains.buildServer.configs.kotlin.*
 import jetbrains.buildServer.configs.kotlin.buildSteps.script
-import jetbrains.buildServer.configs.kotlin.v2019_2.BuildType
-import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.ScriptBuildStep
-import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
+//import jetbrains.buildServer.configs.kotlin.v2019_2.BuildType
+//import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.ScriptBuildStep
+//import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
+import jetbrains.buildServer.configs.kotlin.v2018_2.*
+import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.script
 import java.io.File
 
 //version = "2023.05.4"
